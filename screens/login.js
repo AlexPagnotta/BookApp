7e42e1b9c174405b8a3a4a8fbd7aa1d4a497d6f2
Alex <alex.pagnotta@outlook.it>
2020-03-27 13:38:38 +0100

    Added Validation with Formik and Yup

--- screens/login.js
+++ screens/login.js
@@ -1,30 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import {
-  StyleSheet,
-  ActivityIndicator,
-  TouchableWithoutFeedback,
-  Keyboard
+  StyleSheet, ActivityIndicator, TouchableWithoutFeedback, Keyboard
 } from 'react-native'
 import { connect } from 'react-redux'
 import { actions, States } from '../store'
 import { Button, Input,Layout,Text ,Icon } from '@ui-kitten/components';
+import { Formik } from 'formik'
+import * as Yup from 'yup'
 
 class LoginScreen extends Component {
+
     constructor(props) {
       super(props)
   
       //Init local state
-      this.state = {
-        username: '',
-        password: '', 
+      this.state = { 
         secureTextEntry: true
       }
+
     }
   
     render() {
 
+      //Props from Redux
       const { loading, executeLogin } = this.props
 
+      //Icon Hide Password Methods
       const passwordEyeIcon = (style) => (
         <Icon {...style} name={this.state.secureTextEntry ? 'eye-off' : 'eye'}/>
       );
@@ -32,14 +33,24 @@ class LoginScreen extends Component {
       const onSecureTextIconPress = () => {
         this.setState({secureTextEntry : !this.state.secureTextEntry});
       };
+
+      //Validation Form
+      const validationSchema = Yup.object().shape({
+        username: Yup.string()
+          .label('Username')
+          .required('Username cannot be empty'),
+        password: Yup.string()
+          .label('Password')
+          .required('Password cannot be empty')
+          .min(5, 'Password must have at least 5 characters ')
+      })
   
-      // show only loading indicator if loading state is true
+      // Loading Indicator on Loading
       if (loading) {
         return <ActivityIndicator />
       }
   
-  
-      // display login screen
+      // Login Screen
       return (
         <TouchableWithoutFeedback 
           onPress={ () => { Keyboard.dismiss() }}
@@ -49,27 +60,52 @@ class LoginScreen extends Component {
             <Layout style={styles.logoContainer}>
               <Text category='h1'>BookApp</Text>
             </Layout>
-            <Layout style={styles.inputsContainer}>
-              <Input style={styles.input}
-                label='Email'
-                onChangeText={username => this.setState({ username })}
-                value={this.state.username}
-              />
-              <Input
-                icon={passwordEyeIcon}
-                label='Password'
-                secureTextEntry={this.state.secureTextEntry}
-                onIconPress={onSecureTextIconPress}
-                onChangeText={password => this.setState({ password })}
-                value={this.state.password}
-              />
-            </Layout>
-            <Layout style={styles.buttonsContainer}>
-              <Button style={styles.button}
-                onPress={() => executeLogin(this.state.username, this.state.password)}>
-                  LOGIN
-              </Button>     
-            </Layout>      
+            <Formik 
+              initialValues={{ username: '', password: '' }}
+              onSubmit={values => {executeLogin(values)}}
+              validationSchema={validationSchema}>
+              {({ 
+                handleChange, 
+                handleBlur, 
+                values, 
+                handleSubmit, 
+                errors, 
+                touched, 
+                isValid }) => (
+              <Fragment>
+                <Layout style={styles.inputsContainer}>
+                  <Input style={styles.input}
+                    name='username'
+                    label='Username'
+                    onChangeText={handleChange('username')}
+                    value={values.username}
+                    caption={touched.username ? errors.username : ''}
+                    onBlur={handleBlur('username')}
+                    status={touched.username && errors.username ? 'danger' : ''}
+                  />
+                  <Input
+                    icon={passwordEyeIcon}
+                    name='password'
+                    label='Password'
+                    secureTextEntry={this.state.secureTextEntry}
+                    onIconPress={onSecureTextIconPress}
+                    onChangeText={handleChange('password')}
+                    value={values.password}
+                    caption={touched.password ? errors.password : ''}
+                    onBlur={handleBlur('password')}
+                    status={touched.password && errors.password ? 'danger' : ''}
+                  />
+                </Layout>
+                <Layout style={styles.buttonsContainer}>
+                  <Button style={styles.button}
+                    onPress={handleSubmit}
+                    disabled={!isValid}>
+                      LOGIN
+                  </Button>     
+                </Layout>   
+              </Fragment> 
+              )}
+            </Formik>                        
           </Layout>
         </TouchableWithoutFeedback>
       )
@@ -111,8 +147,15 @@ class LoginScreen extends Component {
     
     // inject actions
     dispatch => ({ 
-      executeLogin: (username, password) =>
-        dispatch(actions.authentication.login(username, password))
+      executeLogin: (values) => {
+
+        if (values.username.length > 0 && values.password.length > 0) {
+          dispatch(actions.authentication.login(values.username, values.password))
+        }
+        else{
+          alert('error')
+        } 
+      }       
     })
 
   )(LoginScreen)
