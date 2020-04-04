511a0b2f4db7e3ff2252a9a0b074dbdffc924cdb
Alex <alex.pagnotta@outlook.it>
2020-04-04 17:59:39 +0200

    Implementing search function

--- screens/search.js
+++ screens/search.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
-import {StyleSheet} from 'react-native'
+import React, { Component,Fragment } from 'react'
+import {StyleSheet, ActivityIndicator,FlatList} from 'react-native'
 import { connect } from 'react-redux'
 import { actions, States } from '../store'
-import { Icon, Tab, Layout, Text,Button } from '@ui-kitten/components';
+import { Icon, Tab, Layout, Text,Button, Input } from '@ui-kitten/components';
+import { Formik } from 'formik'
 
 class SearchScreen extends Component {
 
@@ -16,12 +17,38 @@ class SearchScreen extends Component {
 
   render() {
 
-    const { } = this.props
+    const { foundBooks, isLoading, callError, searchBook} = this.props
     
     
     return (
       <Layout style={styles.container}>
-        <Text>Search</Text>
+       <Formik 
+              initialValues={{ searchText: '' }}
+              onSubmit={values => {
+                searchBook(values.searchText)
+              }}>
+              {({  values, handleChange,handleSubmit }) => (
+              <Fragment>
+                <Input
+                    name='searchText'
+                    label='Search'
+                    onChangeText={handleChange('searchText')}
+                    value={values.password}
+                />
+                <Button
+                  onPress={handleSubmit}
+                  disabled={isLoading}>
+                    {isLoading ? 'Loading...': 'Search'}
+                </Button>   
+              </Fragment> 
+              )}
+            </Formik>  
+            <FlatList
+              data={foundBooks}
+              renderItem={({ item }) => <Text>{item.volumeInfo.title}</Text>}
+              keyExtractor={item => item.id}
+              horizontal={false}
+            />
       </Layout>
     )
   }
@@ -37,9 +64,15 @@ export const Search = connect(
 
   // inject states to props
   (state: States) => ({
+    foundBooks: state.search.foundBooks,
+    isLoading: state.search.isLoading,
+    callError: state.search.callError
   }),
   
   // inject actions to props
   dispatch => ({
+    searchBook:async (searchText) =>{
+      await dispatch(actions.search.searchBook(searchText))
+    }
   })
 )(SearchScreen)
--- store/module/search/actions.js
+++ store/module/search/actions.js
@@ -1,5 +1,6 @@
 import * as costants from './constants'
 import axios from 'axios';
+import GoogleBookService from '../../../apiServices/googleBookService';
 
 /**
 * method that execute search
@@ -8,7 +9,36 @@ export const searchBook = (searchText: String) => {
 
   return async (dispatch, getState) => {
 
-    var foundBooks = []; //TODO: Search books
+    //Start getBooks Action
+    dispatch({
+      type: costants.SEARCH_SEARCH_BOOK
+    })
+
+    try{
+
+      var searchBooksPromise = GoogleBookService.searchBook(searchText); 
+
+      var response = await searchBooksPromise;
+    
+      dispatch({
+        type: costants.SEARCH_SEARCH_BOOK_SUCCESS,
+        payload: {
+          foundBooks: response.items == null ? [] : response.items
+        }
+      })
+      
+    }
+    catch (error) {
+
+      //Error on getBooks
+      dispatch({
+        type: costants.SEARCH_SEARCH_BOOK_ERROR,
+        payload: {
+          callError: error.errorMessage
+        }
+      })
+    }
+
   }
 }
 
