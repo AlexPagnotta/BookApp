f8610c4ad14dc49c4679329e86d3aac5a970aac1
Alex <alex.pagnotta@outlook.it>
2020-04-12 16:24:06 +0200

    Refactor

--- apiServices/baseService.js
+++ apiServices/baseService.js
@@ -9,31 +9,37 @@ import {store,  actions } from '../store';
   * Request Wrapper with default success/error actions
   * @param {*} options 
   * @param {*} isHeader 
+  * @param {*} isGoogleBooksApi 
   */
 const request = async function (options, isHeader = true, isGoogleBooksApi = false) {
 
-
   let authToken = null;
 
+  //If an header is requested, get it from the secure store
   if (isHeader) {
-    //authToken = await SecureStore.getItemAsync(globalCostants.TOKEN_KEY); //TODO: Reimplement 
+    authToken = await SecureStore.getItemAsync(globalCostants.TOKEN_KEY); 
   }
 
+  //Create the client
   const client = axios.create({
     baseURL: isGoogleBooksApi ? globalCostants.API_URL_GOOGLE_BOOKS : globalCostants.API_URL,
     headers: isGoogleBooksApi ? {} : {'Authorization': 'Bearer '+ authToken}  
   });
 
-  //Interceptors to handle success and errors
+  //Define nterceptors to handle success and errors
   client.interceptors.response.use(  (response) => {
 
-    console.debug('Call Success: ' + response.config.url);
+    
+    console.debug('API Call Success: ' + response.config.url);
 
+    //Return data of the response
     return response.data;
 
   }, (error) => {
 
-    //Get the request
+    //Handle errors of the response
+
+    //Get the request from the error
     const request = error.config;
 
     console.debug('Request Failed:', error, request); 
@@ -42,24 +48,26 @@ const request = async function (options, isHeader = true, isGoogleBooksApi = fal
 
     var response = error.response;
 
-    //Reeturn message if BadRequest
+    //Handle 400 BadRequest case and get an optional message returned
     if(response.status === 400){
 
       errorMessage = response.data.message;
 
-      
     }
-    //Try refresh token and retry call, logout if does not work
+
+    //THandle 401 Unathenticated case, try to refresh token and eventually logout
     else if(response.status === 401){
 
       return handleTokenRefresh(client, request);
 
     } 
+
+    //Set default error message for the others status code
     else{
       errorMessage = 'Error while executing Action.';
     }
 
-    //Build error object
+    //Build error object to return 
     var error = {
       error, errorMessage
     }
@@ -68,27 +76,41 @@ const request = async function (options, isHeader = true, isGoogleBooksApi = fal
 
   });
 
+  //Return client
   return client(options);
 }
 
+ /**
+  * Method to handle the 401 status code case, and to refresh the token
+  * @param {*} client 
+  * @param {*} request 
+  */
 const handleTokenRefresh = async function (client, request) {
 
   try{
 
      //If a try has already been made, return to login
      if(request._retry){
-      //Execute logout
+
+      console.debug('Refresh Token Failed, Logging out');
+
+      //A try has already been made, execute logout
       store.dispatch(actions.authentication.logout());
       return;
+
     }
     
-    //Avoid loop
+    //Set property to true to avoid loop
     request._retry = true;
 
-    //Get refresh token
+    console.debug('Refreshing token after 401 response');
 
+    //Get refresh token from secure store
     var refreshToken = await SecureStore.getItemAsync(globalCostants.REFRESH_TOKEN_KEY)
-    console.debug('Get Refresh Token: ' + refreshToken);
+
+    console.debug('Found Refresh Token: ' + refreshToken);
+
+    console.debug('Refreshing Tokens...');
 
     var refreshTokenResponse = await client.post('refreshToken/' + refreshToken);
 
@@ -103,7 +125,9 @@ const handleTokenRefresh = async function (client, request) {
     // Change Authorization header
     request.headers['Authorization'] = 'Bearer ' + refreshTokenResponse.token;
 
-    console.debug('Set token on call: ' + request.headers['Authorization']);
+    console.debug('Set new access token on call header: ' + request.headers['Authorization']);
+
+    console.debug('Retry call with new token');
 
     // Return request object with Axios.
     return client(request); 
@@ -111,7 +135,8 @@ const handleTokenRefresh = async function (client, request) {
   }
   catch(error){
 
-    console.debug(error);
+    console.debug('Logging Out, Error while trying to refresh token' + error);
+
     //Execute logout
     store.dispatch(actions.authentication.logout());
 
